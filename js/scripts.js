2139676a9ca0296eb42e2ceedb2eace84a7a4d19
Tyler Kostelak <tkostelak@gmail.com>
2018-01-19 11:50:56 -0800

    variable divisibleByThree is set to false by default, but if userInput is divided by three with no remainder, then it sets to true.

--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,31 @@
 var userInput;
 var numberZero =["0"];
 var numberOne = ["1"];
-var divisibleByThree =
+var divisibleBythree = false;
 var numberArray = [];
 var containsZero;
 var containsOne;
 
 
 var containsZero = userInput;
-numberZero.forEach(function(zero) {
-  if (userInput === numberZero) {
-    containsZero = true;
-    };
-  });
+  numberZero.forEach(function(zero) {
+    if (userInput === numberZero) {
+      containsZero = true;
+      };
+    });
 
 var containsOne = userInput;
-numberOne.forEach(function(one) {
-  if (userInput === numberOne) {
-    containsOne = true;
+  numberOne.forEach(function(one) {
+    if (userInput === numberOne) {
+      containsOne = true;
   };
 });
 
-
+var divisibleBythree = function(userInput) {
+  if ((userInput % 3 === 0)) {
+    divisibleBythree = true;
+  }
+}
 //user interface logic
 $(document).ready(function() {
   $("#formNumberwang").submit(function(event) {
