6cb00f64a877bcc8b3f5b2d9ee1a9ecdb414614a
Tyler Kostelak <tkostelak@gmail.com>
2018-01-19 11:32:23 -0800

    variables containsZero and containsOne assesses if userInput array contains 0 or 1 by using a boolean

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,39 @@
 // business logic
 var userInput;
-var numberZero;
-var numberOne;
-var divisibleByThree;
+var numberZero =["0"];
+var numberOne = ["1"];
+var divisibleByThree =
+var numberArray = [];
+var containsZero;
+var containsOne;
 
+
+var containsZero = userInput;
+numberZero.forEach(function(zero) {
+  if (userInput === numberZero) {
+    containsZero = true;
+    };
+  });
+
+var containsOne = userInput;
+numberOne.forEach(function(one) {
+  if (userInput === numberOne) {
+    containsOne = true;
+  };
+});
 
 
 //user interface logic
 $(document).ready(function() {
   $("#formNumberwang").submit(function(event) {
-    var userInput = $("#formNumberwang").val();
-    var userInput = userInput.split();
-    alert (typeof(userInput));
+    userInput = $("#number").val();
+      alert (userInput);
+        userInput = userInput.split("");
+          alert (typeof(userInput));
+            alert (userInput);
+
     event.preventDefault();
-  
   });
+
+
 });
