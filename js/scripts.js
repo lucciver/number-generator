3f6048757a45c934ac335951a2f3da7d3152954f
Tyler Kostelak <tkostelak@gmail.com>
2018-01-19 10:28:25 -0800

    javascript sets value that the user puts in the form equal to variable userInput, and also converts the number from a string into an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 // business logic
-
+var userInput;
+var numberZero;
+var numberOne;
+var divisibleByThree;
 
 
 
 //user interface logic
 $(document).ready(function() {
+  $("#formNumberwang").submit(function(event) {
+    var userInput = $("#formNumberwang").val();
+    var userInput = userInput.split();
+    alert (typeof(userInput));
+    event.preventDefault();
+  
+  });
 });
